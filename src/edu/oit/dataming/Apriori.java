8aa7ecb334a7c5f090fc578f87e50dba837d5442
samzhao2008 <oracle.zhao2008@gmail.com>
2013-10-16 19:28:51 -0700

    I finish my own part

--- src/edu/oit/dataming/Apriori.java
+++ src/edu/oit/dataming/Apriori.java
@@ -3,20 +3,23 @@ package edu.oit.dataming;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Apriori {
 	protected int min_sup = 2;
-    protected ArrayList<int[]> rawTransaction = new  ArrayList<int[]>();
+    protected ArrayList<char[]> rawTransaction = new  ArrayList<char[]>();
+    protected HashMap<char[], Integer> record = new HashMap<char[], Integer>();
     
     public Apriori(){
     	rawTransaction = this._genTestData();
     } 
     
-    public Apriori(ArrayList<int[]> trans){
+    public Apriori(ArrayList<char[]> trans){
     	rawTransaction = trans;
     }
     
-    public Apriori(ArrayList<int[]> trans, int min_sup){
+    public Apriori(ArrayList<char[]> trans, int min_sup){
     	rawTransaction = trans;
     	this.min_sup = min_sup;
     }
@@ -24,30 +27,102 @@ public class Apriori {
  * This function has the same function as find_frequent_1-itemsets on page 239
  * @return L1 type: HashMap containing the single itemset and it's support number.
  */
-    public HashMap<int[], Integer> getItemFrequency(){
-    	HashMap<int[], Integer> freq = new HashMap<int[], Integer>();
+    public HashMap<char[], Integer> getItemFrequency(){
+    	HashMap<char[], Integer> freq = new HashMap<char[], Integer>();
     	for (int i = 0 ; i < rawTransaction.get(0).length; i++ ){
-    		int tmp[] = new int[rawTransaction.get(0).length];
+    		char tmp[] = new char[rawTransaction.get(0).length];
+    		Arrays.fill(tmp, '0');
     		int count = 0;
-    		tmp[i] = 1;
-    		for (int[] subset: rawTransaction){
-    			if (subset[i] == 1) count++;
+    		tmp[i] = '1';
+    		for (char[] subset: rawTransaction){
+    			if (subset[i] == '1') count++;
     		}
-    		if (count >= this.min_sup)freq.put(tmp, count);
+    		if (count >= this.min_sup){
+    			freq.put(tmp, count);
+    			record.put(tmp, count);
+    			}
     	}
     	return freq;
     }
     
     public void implementApriori(){
-    	HashMap<int[], Integer> output = this.getItemFrequency();
+    	HashMap<char[], Integer> output = this.getItemFrequency();
     	for (int i = 2 ; !output.isEmpty() ; i++){
-    		
+    		output = this.aprioriGenerator(output.keySet(), i);
+    	    System.out.println("L"+(i-1)+":");
+    	    this._printHashMap(output);
     	}
     }
     
-    public HashMap<int[], Integer> filterUnvalid(HashMap<int[], Integer> input){
-    	HashMap<int[], Integer> output = new HashMap<int[], Integer>();
-    	for (int[] key: output.keySet()){
+    public HashMap<char[], Integer> getRecord(){
+    	return this.record;
+    }
+    
+    
+    private HashMap<char[], Integer> aprioriGenerator(Set<char[]> inputSet, int k){
+    	HashSet<String> output = new HashSet<String>();
+    	HashMap<char[], Integer> Ls = new HashMap<char[], Integer>();
+    	HashMap<char[], Integer> Cs = new HashMap<char[], Integer>();
+    	int itemNum = 0;
+    	int i = 0;
+    	int support = 0;
+    	int inputSize = inputSet.size();
+    	String[] itemSet = new String[inputSize];
+    	for (char[] one: inputSet){
+    		if (itemNum == 0)itemNum = one.length;
+    		itemSet[i] = String.valueOf(one);
+    		i++;
+    	}
+    	for (int m = 0 ; m < inputSize; m++){
+    		for (int n = m+1 ; n < inputSize; n++){
+    			String result = this._joinTwoSet(itemSet[m], itemSet[n], k-2);
+    			if (result!=""){
+    				support = this._calculateSupport(result);
+    				Cs.put(result.toCharArray(), support);
+    				if (support >= this.min_sup && !output.contains(result)){
+    					output.add(result);
+    					//System.out.println(result);
+    					Ls.put(result.toCharArray(), support);
+    					record.put(result.toCharArray(), support);
+    				}
+    			}
+    		}
+    	}
+    	return Ls;
+    }
+    
+    public int _calculateSupport(String itemSet){
+    	int count = 0;
+    	int s_toi;
+    	int itemSet_toi = Integer.parseInt(itemSet, 2);
+    	String temp;
+    	for (char[] set: this.rawTransaction){
+    		temp = String.valueOf(set);
+    		s_toi = Integer.parseInt(temp,2);
+    		if ((s_toi & itemSet_toi) == itemSet_toi)count++;
+    	}
+    	return count;
+    }
+    
+    public String _joinTwoSet(String a, String b, int common){
+    	int a_toi =  Integer.parseInt(a, 2);
+    	int b_toi =  Integer.parseInt(b, 2);
+    	int length = a.trim().length();
+    	int afterAnd = a_toi&b_toi;
+    	int afterOr;
+    	String mid = Integer.toBinaryString(afterAnd);
+    	int count = mid.replaceAll("[^1]", "").length();
+    	if (count == common){
+    		afterOr = a_toi | b_toi;
+    		String result = Integer.toBinaryString((int)Math.pow(2,length) | afterOr).substring(1);
+    		return result;
+    	}
+    	else return "";
+    }
+    
+    public HashMap<char[], Integer> filterUnvalid(HashMap<char[], Integer> input){
+    	HashMap<char[], Integer> output = new HashMap<char[], Integer>();
+    	for (char[] key: output.keySet()){
     		if (input.get(key) >= this.min_sup){
     			output.put(key, input.get(key));
     		}
@@ -55,18 +130,18 @@ public class Apriori {
     	return output;
     }
     
-    private ArrayList<int[]> _genTestData(){
-    	ArrayList<int[]> test = new ArrayList<int[]>();
-    	int data[][] = {
-    			        {1,1,0,0,1},
-    			        {0,1,0,1,0},
-    			        {0,1,1,0,0},
-    			        {1,1,0,1,0},
-    			        {1,0,1,0,0},
-    			        {0,1,1,0,0},
-    			        {1,0,1,0,0},
-    			        {1,1,1,0,1},
-    			        {1,1,1,0,0}
+    private ArrayList<char[]> _genTestData(){
+    	ArrayList<char[]> test = new ArrayList<char[]>();
+    	char data[][] = {
+    			        {'1','1','0','0','1'},
+    			        {'0','1','0','1','0'},
+    			        {'0','1','1','0','0'},
+    			        {'1','1','0','1','0'},
+    			        {'1','0','1','0','0'},
+    			        {'0','1','1','0','0'},
+    			        {'1','0','1','0','0'},
+    			        {'1','1','1','0','1'},
+    			        {'1','1','1','0','0'}
     			        };
         for (int i = 0; i < data.length; i++){
         	test.add(data[i]);
@@ -74,9 +149,9 @@ public class Apriori {
     	return test;
     }
     
-    private void _printHashMap(HashMap<int[], Integer> input){
-    	for (int[] key: input.keySet()){
-        	System.out.println("key = :"+ Arrays.toString(key));
+    public void _printHashMap(HashMap<char[], Integer> input){
+    	for (char[] key: input.keySet()){
+        	System.out.print("key = :"+ Arrays.toString(key));
         	System.out.println("value = :" + input.get(key));
         }
     }
--- src/edu/oit/dataming/Main.java
+++ src/edu/oit/dataming/Main.java
@@ -6,43 +6,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
+import org.apache.commons.codec.binary.StringUtils;
+
 public class Main {
 
 	public static void main(String[] args) throws IOException  {
-//		int value1 = Integer.parseInt("0101011", 2);
-//		int value2 = Integer.parseInt("011",2);
-//		int value3 = value1&value2;
-//		System.out.println("value1: "+value1);
-//		System.out.println("value2: "+value2);
-//		System.out.println("value1 & value2 :" + value3);
-		
-		//Excel fOperat = new Excel();
-		//fOperat.readFile();
-//		int test[] = {0,1,2,2,2,8};
-//		System.out.println(Arrays.toString(test));
-//		Map<int[], Integer> entry = new HashMap<int[], Integer>();
-//		int a1[] = {0,1,0,1,0};
-//		int a2[] = {1,1,0,0,1};
-//		entry.put(a1, 3);
-//		entry.put(a2, 4);
-//		for (Map.Entry ent : entry.entrySet()) {
-//			int key[] = (int[])ent.getKey();
-//            System.out.println("Key = " + key[0] + ", Value = " +
-//                               ent.getValue());
-//        }
         Apriori test = new Apriori();
-        char a[] = new char[5];
-        Arrays.fill(a, '0');
-        a[1] = '1';
-        String str = String.valueOf(a);
-        System.out.println(str);
-        String val = "010010";
-        char[] charArray = val.toCharArray();
-		System.out.println(charArray[1]);
-//		int test[] = new int[10];
-//		System.out.println(Arrays.toString(test));
-		
-		
+        test.implementApriori();
+        HashMap<char[], Integer> record = test.getRecord();
+        System.out.println("record: ");
+        test._printHashMap(record);
 	}
 
 }
